4053e0c7393d85243f6d35ca6d66ae9012d95936
OrlandoAlata <orlandoalata@gmail.com>
2025-10-20 13:35:41 -0400

    Final JS Workshop

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello from indexedDB.js!");
+alert("Javascript is linked!");
+
+let code1;
+code1 = 7 + 3;
+
+let code2 = 40 + (40 % 2);
+let code3 = (45 % 6) + 36;
+
+const message1 = "The vault has been secured. The combination is..";
+
+const codeA = "10-40-39";
+
+const codeB = `${code1}-${code2}-${code3};`;
+
+console.log(message1, codeA, codeB);
